6021f7be03a2f25811aea232233493bc6e3e6b1b
Samuele Daminato <samuele.daminato@usi.ch>
2025-02-15 01:56:02 +0100

    restructure folders, map svg, map component

---
+++ my-react-project/client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import WorldMap from "./components/WorldMap";
+
+function App() {
+    return (
+        <div className="App">
+            <WorldMap />
+        </div>
+    );
+}
+
+export default App;
---
+++ my-react-project/server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Server is running...");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
